d367423955ae3af8a0be66a302c5b03e648540f8
NavyaIlla <94620692+NavyaIlla@users.noreply.github.com>
2021-11-19 11:32:09 +0530

    Add files via upload
    Assignment 2

---
+++ Assingment2/Assingment2/Document.cs
@@ -0,0 +1,7 @@
+﻿namespace Assignment2
+{
+	public class Document
+	{
+		public string text = "Assignment";
+	}
+}
---
+++ Assingment2/Assingment2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace Assignment2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Document doc = new Document();
+
+			SignDocumnet(doc);
+
+		}
+		static async void SignDocumnet(Document documentContent)
+		{
+			String content1 = await SantizeDocumentContentAsync(documentContent);
+			String content2 = await HashSantizedDocumentContentAsync(content1);
+			String content3 = await EncryptDigestUsingPrivateKeyAsync(content2);
+		}
+		private static Task<string> SantizeDocumentContentAsync(Document documentContent)
+		{
+			return Task.Run<string>(() =>
+			{
+				return SantizeDocumentContent(documentContent);
+			});
+		}
+		static string SantizeDocumentContent(Document documentContent)
+		{
+			Task.Delay(2000).Wait();
+			return documentContent.text + " Santized ";
+		}
+
+
+
+		private static Task<string> HashSantizedDocumentContentAsync(string santizedDocumentContent)
+		{
+			return Task.Run<string>(() =>
+			{
+				return HashSantizedDocumentContent(santizedDocumentContent);
+			});
+		}
+		static string HashSantizedDocumentContent(string santizedDocumentContent)
+		{
+			Task.Delay(2000).Wait();
+			return santizedDocumentContent + " Hashed ";
+		}
+
+
+		private static Task<string> EncryptDigestUsingPrivateKeyAsync(string digest)
+		{
+			return Task.Run<string>(() =>
+			{
+				return EncryptDigestUsingPrivateKey(digest);
+			});
+		}
+		static string EncryptDigestUsingPrivateKey(string digest)
+		{
+			Task.Delay(2000).Wait();
+			return digest + " Encrypted ";
+		}
+
+	}
+}
