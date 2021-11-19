5fb87846ad092d81f90c0bf4020f6dfcc85ea0da
NavyaIlla <94620692+NavyaIlla@users.noreply.github.com>
2021-11-19 11:42:17 +0530

    Assignment1

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Assignment1
+{
+	public enum OrderState
+	{
+		CRETAED, CONFIRMED, CANCELLED, CLOSED
+	}
+
+	public class Order
+	{
+		public event Action<string> OrderStateChanged;
+		public event Action<string> OrderClosed;
+		string orderId;
+		OrderState currentState;
+		public Order()
+		{
+			orderId = Guid.NewGuid().ToString();
+			currentState = OrderState.CRETAED;
+		}
+		public void ChangeState(OrderState newState)
+		{
+			this.currentState = newState;
+			if (this.currentState == OrderState.CLOSED)
+			{
+				NotifyClosed();
+
+			}
+			else
+			{
+
+				NotifyAll();
+			}
+
+		}
+
+		void NotifyClosed()
+		{
+			if (OrderClosed != null)
+			{
+				OrderClosed.Invoke(orderId);
+			}
+		}
+		void NotifyAll()
+		{
+			if (OrderStateChanged != null)
+			{
+
+				this.OrderStateChanged.Invoke(this.orderId);
+
+			}
+
+
+		}
+
+	}
+	public class AuditSystem
+	{
+		public void SendAudit(string eventData)
+		{
+			Console.WriteLine($"Order closed {eventData}");
+
+		}
+	}
+	public class EmailNotifificationSystem
+	{
+		public void SendMail(string evtData) { Console.WriteLine($"Email Sent  {evtData}"); }
+	}
+	public class SMSNotificationSystem
+	{
+		public void SendSMS(string evtData)
+		{
+			Console.WriteLine($"SMS Sent  {evtData}");
+		}
+	}
+	public class WhatsAppNotificationSystem
+	{
+		public void SendWhatsApp(string eventData)
+		{
+			Console.WriteLine($"Whatsapp sent {eventData}");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			EmailNotifificationSystem _emailSystem = new EmailNotifificationSystem();
+			SMSNotificationSystem _smsSystem = new SMSNotificationSystem();
+			WhatsAppNotificationSystem _whatsappSystem = new WhatsAppNotificationSystem();
+			AuditSystem _auditSystem = new AuditSystem();
+
+			Action<string> _emailObserver = new Action<string>(_emailSystem.SendMail);
+			Action<string> _smsObserver = new Action<string>(_smsSystem.SendSMS);
+			Action<string> _whatsappObserver = new Action<string>(_whatsappSystem.SendWhatsApp);
+			Action<string> _closedObserver = new Action<string>(_auditSystem.SendAudit);
+
+			Order _order1 = new Order();
+			_order1.OrderStateChanged += _emailObserver;
+			_order1.OrderStateChanged += _smsObserver;
+			_order1.OrderStateChanged += _whatsappObserver;
+			_order1.OrderClosed += _closedObserver;
+
+			_order1.ChangeState(OrderState.CONFIRMED);
+			System.Threading.Tasks.Task.Delay(1000).Wait();
+			_order1.ChangeState(OrderState.CANCELLED);
+			System.Threading.Tasks.Task.Delay(3000).Wait();
+			_order1.ChangeState(OrderState.CONFIRMED);
+			System.Threading.Tasks.Task.Delay(5000).Wait();
+			_order1.ChangeState(OrderState.CLOSED);
+		}
+	}
+}
